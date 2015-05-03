0f93c6119c9d7e89b2398ea8fad7526789492b76
Kenneth Lim <kennethlimjf@gmail.com>
2015-05-04 00:56:04 +0800

    Integrate with backend API

--- app/models/driver.js
+++ app/models/driver.js
@@ -2,26 +2,29 @@ import DS from "ember-data";
 import Ember from "ember";
 
 export default DS.Model.extend({
-  name:             DS.attr(),
+  firstName:        DS.attr('String'),
+  lastName:         DS.attr('String'),
   nickname:         DS.attr("String"),
   shortDesc:        DS.attr("String"),
   description:      DS.attr("String"),
   phone:            DS.attr("String"),
   email:            DS.attr("String"),
-  whatsapp:         DS.attr("String"),
-  wechat:           DS.attr("String"),
-  facebook:         DS.attr("String"),
+  sourceUrl:        DS.attr("String"),
+  videoUrl:         DS.attr("String"),
+  facebookUrl:      DS.attr("String"),
+  wechatId:         DS.attr("String"),
+  whatsappId:       DS.attr("String"),
   available:        DS.attr("Boolean"),
-  drivingYears:     DS.attr("String"),
-  vehicle:          DS.attr(),
-  area:             DS.attr(),
-  languageWritten:  DS.attr(),
-  languageSpoken:   DS.attr(),
+  drivingYears:     DS.attr("Number"),
+  languagesWritten: DS.attr(),
+  languagesSpoken:  DS.attr(),
+  vehicles:         DS.attr(),
+  cities:           DS.attr(),
   avatarUrl:        DS.attr("String", { defaultValue : "http://www.angkortuktuk.net/Savuth%20portrait.jpg" }),
   bgUrl:            DS.attr("String", { defaultValue : "http://www.epuzzled.net/New%20Folder%20(5)/tuk%20tuk%20Bangkok%20562%20kb.JPG"}),
 
   fullName: Ember.computed("name", function() {
-    return this.get("name.first") + " " + this.get("name.last");
+    return this.get("firstName") + " " + this.get("lastName");
   }),
 
   mailToEmail: Ember.computed("email", function() {
