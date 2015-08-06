bb7329a9fe0c61764c11ca852e302831f94be02f
honglin <yanghl22@gmail.com>
2015-08-07 00:29:10 +0800

    Disable the Login route and Logout route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+actions: {
+        facebookLogin: function() {
+            this.get('session').authenticate('simple-auth-authenticator:torii', 'facebook-oauth2').then(function () {
+                //do sth here!
+            });
+            return;
+        },
+        googleLogin: function() {
+            this.get('session').authenticate('simple-auth-authenticator:torii', 'google-oauth2').then(function () {
+                //do sth here!
+            });
+            return;
+        }
+    }
+});
