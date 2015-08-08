838d5803231798a834ace156307af4c99a1cba34
honglin <yanghl22@gmail.com>
2015-08-08 14:45:02 +0800

    Add terms and privacy in footer

--- app/router.js
+++ app/router.js
@@ -13,6 +13,9 @@ Router.map(function() {
   this.route("team", { path: "our-team" });
   this.route("faq", { path: "faq" });
   this.route("feedback", { path: "feedback" });
+  this.route("terms", { path: "terms" });
+  this.route("privacy", { path: "privacy" });
+
   this.route('protected');
 });
 
